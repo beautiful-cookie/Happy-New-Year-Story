e93d19b8fa87706c9b720c7e3603dcee4a5ca85f
beautiful-cookie <maxim.khadeev@gmail.com>
2022-12-27 22:10:33 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,83 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div id="app"> 
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box; 
+
+  ::-webkit-scrollbar {
+    display: none; 
+  }
+} 
+
+body {
+  position: relative; 
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  background: url('@/assets/santa-bg3.png') center/100% 110% no-repeat; 
+  background-attachment: fixed;
+  z-index: 1; 
+
+  &::after {
+    content: ''; 
+    position: absolute; 
+    top: 0; 
+    left: 0; 
+    width: 100vw; 
+    min-height: 100vh;
+    height: 100%; 
+    background: rgba(0, 0, 0, 0.6); 
+    z-index: 2; 
+  }
+} 
+
+#app { 
+  display: flex; 
+  justify-content: center; 
+  width: 70%; 
+  max-width: 700px;
+  min-height: 100vh; 
+  height: 100%; 
+  z-index: 3; 
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@media screen and (max-width: 1000px) {
+  body {
+    background-image: url('@/assets/santa-bg.png'); 
   }
 }
+
+@media screen and (max-width: 800px) {
+  #app {
+    width: 90%; 
+  }
+}
+
+@media screen and (max-width: 670px) {
+  body {
+    background-image: url('@/assets/santa-bg2.png'); 
+  }
+
+  #app {
+    width: 100%; 
+  }
+}
+
+@media screen and (max-width: 500px) {
+  body {
+    background-image: url('@/assets/phone-bg.jpg'); 
+  }
+}
+</style>
+
+<style> 
+/* all used fonts */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap'); 
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content-wrapper">
+    <span class="title">С Новым 2023 Годом!</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'HomeView', 
+}
+</script> 
+
+<style lang="scss" scoped>
+.content-wrapper {
+  display: flex; 
+  justify-content: center; 
+  align-items: center;
+  flex-direction: column; 
+  width: 100%; 
+  height: 100%; 
+  z-index: 4; 
+
+  .title { 
+    margin-top: 35px;
+    color: white; 
+    text-shadow: 0 0 7px white; 
+    text-align: center; 
+    font-size: 50px; 
+    font-weight: 500; 
+    font-family: 'Montserrat', sans-serif;
   }
 }
-</script>
+
+
+@media screen and (max-width: 670px)  {
+  .content-wrapper {
+    .title {
+      font-size: 40px; 
+    }
+  }
+}
+
+@media screen and (max-width: 500px)  {
+  .content-wrapper {
+    .title {
+      font-size: 30px; 
+    }
+  }
+}
+
+</style>
