f1da94953657f5a5f6d91d16762d66f99b71c139
beautiful-cookie <maxim.khadeev@gmail.com>
2022-12-30 02:50:09 +0300

    photos adde

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="cards-content">
-    <div class="card-wrapper">
-      
+    <div class="card-wrapper" v-for="picture of picturesArr" :key="picture">
+      <img :src="require(`../assets/nature-pictures/photo(${picture}).jpg`)" alt="Картинка не загрузилась">
     </div>
+
   </div>
 </template>
 
@@ -10,30 +11,48 @@
 export default {
   data() {
     return {
-      picturesArr: null, 
+      picturesArr: [] 
     }
   }, 
   created() {
-    this.picturesArr = [] 
+    this.getPictures()  
   }, 
   methods: {
     getPictures() {
-      while (1==1) { 
-        // let hrefToPic = '@/assets/pictures/'
-        // if (hrefToPic) {
-        //   this.picturesArr.push(hrefToPic)
-        // }
+      for (let i = 1; i<13; i++) {
+        this.picturesArr.push(i)
       }
-      
     }
   }
-
 }
 </script>
 
 <style lang="scss" scoped>
 .cards-content { 
+  display: flex; 
+  justify-content: center; 
+  align-items: center; 
+  flex-wrap: wrap; 
   width: 100%; 
+  margin-top: 20px; 
+  gap: 10px; 
+  max-height: 100vh; 
+  overflow: scroll; 
+
+  .card-wrapper {
+    flex: 45%; 
+    max-width: 40%; 
+    max-height: 300px; 
+    padding: 8px; 
+    border: 2px solid white; 
+    border-radius: 5px;
+    overflow: scroll; 
+
+    img {
+      max-width: 100%; 
+      max-height: 100%; 
+    }
+  }
 }
 </style>
 
