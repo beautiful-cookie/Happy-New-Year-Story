8f06a80a52953ef8d7406f285ecada26cb86ccf6
beautiful-cookie <maxim.khadeev@gmail.com>
2022-12-31 02:57:04 +0300

    config cjanges

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true, 
+  publicPath: '/shards-of-our-memories/', 
   chainWebpack: config => { config.plugin('html').tap(args => { args[0].title = 'Memory shards'; return args; }); }
 })
